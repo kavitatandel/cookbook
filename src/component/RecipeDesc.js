e630a9c4d9d8e99efb903227df098477d7963200
kavitatandel <kavita2810@gmail.com>
2022-01-10 23:59:44 +0100

    updated ShareRecipe and added LoadingCircular

--- src/component/RecipeDesc.js
+++ src/component/RecipeDesc.js
@@ -5,12 +5,28 @@ import RecipeDescGrid from './RecipeDescGrid';
 import { useParams } from 'react-router';
 import { Client } from './Client'
 import { useState, useEffect } from 'react';
+import LoadingCircular from './LoadingCircular';
 
 const DivMain = styled.div`
 width: 100%;
 margin: 50px auto 0px auto;
 `;
 
+const DivMainErrorProgress = styled.div`
+height:100vh;
+min-height:800px;
+width: auto;
+display: flex;
+align-items: center;
+justify-content: center;
+padding-bottom: 60px;
+`;
+
+//controls the size of the circular progress bar
+const DivCircularProgressbar = styled.div`
+width: 300px;
+height: 300px;
+`;
 
 const RecipeDesc = () => {
 
@@ -20,14 +36,18 @@ const RecipeDesc = () => {
     const [error, setError] = useState(false);
 
     const fetchRecipeById = async () => {
-        console.log(recipeId)
-        await Client.getEntry(recipeId)
-            .then(response => {
-                console.log(response)
-                setRecipe(response)
-                setLoading(false)
-            })
-            .catch(error => console.log(error));
+        try {
+            await Client.getEntry(recipeId)
+                .then(response => {
+                    console.log(response)
+                    setRecipe(response)
+                    setLoading(false)
+                })
+        }
+        catch (error) {
+            setError(true);
+            console.log(error)
+        };
     }
 
     useEffect(() => {
@@ -35,8 +55,22 @@ const RecipeDesc = () => {
     }, [])
 
 
-    if (loading) return <h1>Page is loading</h1>
-    if (error) return <h1>There is an error occured. Please try again later....</h1>
+    //if (loading) return <h1>Page is loading</h1>
+    if (error) return (
+        <DivMainErrorProgress>
+            <h1>There is an error occured. Please try again later....</h1>
+        </DivMainErrorProgress>
+    )
+
+    if (loading) return (
+        <DivMainErrorProgress>
+            <DivCircularProgressbar>
+                <LoadingCircular />
+            </DivCircularProgressbar>
+        </DivMainErrorProgress>
+    )
+
+
     return (
         <>
             <DivMain>
